17c332a280286385c782a78aa832abaec892a6c4
Guilherme-FCM <guilherme_fcm@hotmail.com>
2023-03-22 20:17:03 -0300

    Add random signos activity

--- app/src/main/java/com/example/treinandoamemria/FormularioActivity.java
+++ app/src/main/java/com/example/treinandoamemria/FormularioActivity.java
@@ -2,13 +2,21 @@ package com.example.treinandoamemria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class FormularioActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener((view -> {
+            startActivity( new Intent(this, RandomSignosActivity.class) );
+        }));
     }
 }
