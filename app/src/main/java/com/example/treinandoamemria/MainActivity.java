9341012e8afce9c5c264f62f7573c251d9cce363
Guilherme-FCM <guilherme_fcm@hotmail.com>
2023-03-22 19:57:40 -0300

    Init Project and create presentation activity

---
+++ app/src/main/java/com/example/treinandoamemria/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.treinandoamemria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    private final int time = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(() -> {
+            startActivity( new Intent(this, FormularioActivity.class) );
+        }, time);
+    }
+}
